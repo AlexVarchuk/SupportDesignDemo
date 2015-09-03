60696f3c0116f4e262dfc60223a0e5febab77195
AlexVarchuk <82346920f>
2015-09-03 22:23:42 +0300

    add log start third activity

--- app/src/main/java/com/example/d_rom/supportdesigndemo/ThirdActivity.java
+++ app/src/main/java/com/example/d_rom/supportdesigndemo/ThirdActivity.java
@@ -1,4 +1,15 @@
 package com.example.d_rom.supportdesigndemo;
 
-public class ThirdActivity {
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class ThirdActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_third);
+        Log.d("TAG", "start third activity");
+    }
 }
